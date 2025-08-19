d85e79d15a3df8954899efd119602f3442da85c1
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-20 03:12:31 +0900

    initial

---
+++ src/components/box.jsx
@@ -0,0 +1,5 @@
+export const Box = () => {
+  return (
+    <div className="w-25 h-25 bg-gray-50 border-1 border-red-500">Box</div>
+  );
+};
---
+++ src/components/round.jsx
@@ -0,0 +1,7 @@
+export const Round = () => {
+  return (
+    <div className="w-25 h-25 rounded-full bg-gray-50 border-1 border-red-500">
+      Round
+    </div>
+  );
+};
