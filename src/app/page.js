6face58c96f07b0c2dfbe622b3cdf716f96327ae
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-20 09:14:28 +0800

    added

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ const Home = () => {
       <Box title="box1"></Box>
       <Box color="gray" isBox={true}></Box>
       <Card image="https://www.seavees.com/cdn/shop/files/Womens_Cardinal_Sneaker_Latte-WCCSC23NSP_LAT_Side.jpg?v=1688074605"></Card>
+      <Card image="https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/6040adb7-d8f8-40a2-ad17-d0c52c6971c1/NIKE+C1TY.png"></Card>
     </div>
   );
 };
