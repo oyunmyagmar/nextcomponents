a8deafcd939064228f51b1a25ddaa7104c214891
oyunmyagmar <25LP4018@nest.edu.mn>
2025-09-01 03:08:31 +0900

    added

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,7 @@
 export const Card = (props) => {
   return (
-    <div className="w-100 h-100 flex gap-2">
-      <img
-        className="w-full h-100"
-        src={props.image}
-        alt="picture of sneaker"
-      ></img>
+    <div>
+      <img src={props.image} alt="picture of sneaker"></img>
     </div>
   );
 };
-// <div>
-//   <img src={"../../images/nike.webp1"} alt="" width={200} height={200} />
-//   image
-// </div>
